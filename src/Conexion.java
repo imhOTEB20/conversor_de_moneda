40e6c96ce3e1f61c7fd3d15da875b770e44f67be
Cristian Geronimo <crisalbertoaa20@gmail.com>
2024-04-24 04:03:50 -0300

    Se creo la clase 'Conexion.java', esta permite conectarse con la api de 'https://v6.exchangerate-api.com' y tiene un metodo que permite hace un get de acuerdo a la moneda como base que se pasa como parametro

---
+++ src/Conexion.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conexion {
+    private final String key;
+    private String url;
+    private final HttpClient client;
+    private final String DIRECCION = "https://v6.exchangerate-api.com/v6/";
+
+    public Conexion(String key) {
+        this.key = key;
+        this.client = HttpClient.newHttpClient();
+    }
+
+    private void generateURL(String base_code) {
+        this.url = String.format(this.DIRECCION,this.key,"/latest/", base_code);
+    }
+
+    public String getResponse(String base_code) throws IOException, InterruptedException {
+        this.generateURL(base_code);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(this.url))
+                .build();
+
+        HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+}
